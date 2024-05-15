e7f6879b01cc9c0bc262e9492411be5ec93dd291
Alexandr Dementev <feralist2111@yandex.ru>
2024-05-15 20:01:39 +0800

    Промежуточные изменения

---
+++ src/Pages/Main.tsx
@@ -0,0 +1,13 @@
+import Container from "@/Components/Container";
+
+const MainPage = () => {
+    return (
+        <Container>
+            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        </Container>
+    )
+
+
+};
+
+export default MainPage;
