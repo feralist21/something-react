287d68c8762338d978821341b4aba0788c5e8601
Alexandr Dementev <feralist2111@yandex.ru>
2024-05-18 12:30:37 +0800

    Добавляет предварительную разметку списка задач

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,5 +1,19 @@
+import Checkbox from "@/Components/Checkbox";
+
 const MainPage = () => {
-    return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+    return (
+        <div className='p-8 bg-gray-300 flex flex-col gap-y-2 rounded-lg w-[700px] mx-auto'>
+            <div className='p-4 bg-white rounded-lg flex gap-x-3 items-center'>
+                <Checkbox/>
+                <div className='flex flex-col gap-y-1 flex-grow'>
+                    <h2 className='text-lg font-medium'>Title Todo</h2>
+                    <p className='text-sm'>Deadline: date</p>
+                </div>
+                <button className="p-2 bg-blue-300 text-white text-sm">Edit</button>
+                <button className='p-2 bg-red-400 text-white text-sm'>Delete</button>
+            </div>
+        </div>
+    );
 };
 
 export default MainPage;
