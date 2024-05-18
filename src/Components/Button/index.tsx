e78e7ed555b61a92115efd3d1e7edc313d0ece80
Alexandr Dementev <feralist2111@yandex.ru>
2024-05-18 13:03:08 +0800

    Добавляет  компонет кнопки

---
+++ src/Components/Button/index.tsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+import { ComponentProps, ElementType, ReactNode } from 'react';
+
+type ButtonOwnProps<E extends ElementType = ElementType> = {
+    children: ReactNode;
+    className?: string;
+    view?: 'primary' | 'secondary';
+    as?: E;
+};
+
+type ButtonProps<E extends ElementType> = ButtonOwnProps<E> &
+    Omit<ComponentProps<E>, keyof ButtonOwnProps>;
+
+const defaultElement = 'button';
+
+// TODO: добавить forwardRef
+export default function Button<E extends ElementType = typeof defaultElement>({
+    children,
+    as,
+    className,
+    view,
+    ...otherProps
+}: ButtonProps<E>) {
+    const classes = clsx(
+        className,
+        'block p-2 text-sm',
+        view === 'primary' ? 'bg-blue-300 text-white' : '',
+        view === 'secondary' ? 'bg-red-400 text-white' : '',
+    );
+    const TagName = as || defaultElement;
+
+    return (
+        <TagName className={classes} {...otherProps}>
+            {children}
+        </TagName>
+    );
+}
--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,16 +1,17 @@
-import Checkbox from "@/Components/Checkbox";
+import Button from '@/Components/Button';
+import Checkbox from '@/Components/Checkbox';
 
 const MainPage = () => {
     return (
         <div className='p-8 bg-gray-300 flex flex-col gap-y-2 rounded-lg w-[700px] mx-auto'>
             <div className='p-4 bg-white rounded-lg flex gap-x-3 items-center'>
-                <Checkbox/>
+                <Checkbox />
                 <div className='flex flex-col gap-y-1 flex-grow'>
                     <h2 className='text-lg font-medium'>Title Todo</h2>
                     <p className='text-sm'>Deadline: date</p>
                 </div>
-                <button className="p-2 bg-blue-300 text-white text-sm">Edit</button>
-                <button className='p-2 bg-red-400 text-white text-sm'>Delete</button>
+                <Button view='primary'>Edit</Button>
+                <Button view='secondary'>Delete</Button>
             </div>
         </div>
     );
