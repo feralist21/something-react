7926f22f3aa02d9ff2352521940c199b1971449c
Alexandr Dementev <ad@adictgroup.ru>
2024-05-28 08:18:04 +0800

    Добавляет стрелку в селект

--- src/Components/Select/index.tsx
+++ src/Components/Select/index.tsx
@@ -1,10 +1,10 @@
 import clsx from 'clsx';
 import { FC, useState, useEffect } from 'react';
 
-interface SelectItem {
+type SelectItem = {
     label: string;
     id: number;
-}
+};
 
 interface SelectProps {
     className?: string;
@@ -37,11 +37,25 @@ const Select: FC<SelectProps> = ({ className, placeholder, items, onChange, valu
     return (
         <div className={clsx('relative', className)}>
             <button
-                className='w-full flex items-center gap-x-3 py-2 px-2 text-sm rounded-sm bg-white border-2 border-blue-300 text-black min-h-9'
+                className='w-full flex items-center justify-between gap-x-3 py-2 px-2 text-sm rounded-sm bg-white border-2 border-blue-300 text-black min-h-9'
                 type='button'
                 onClick={toggling}
             >
                 {selectedOption ? selectedOption.label : placeholder}
+                <svg
+                    className={clsx('w-4 h-4', isOpen ? 'rotate-180' : '')}
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                >
+                    <path
+                        d='M12 4v16m0 0 6-6m-6 6-6-6'
+                        stroke='#000000'
+                        stroke-width='1.5'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                    />
+                </svg>
             </button>
             {isOpen && (
                 <ul className='w-full absolute flex flex-col bg-slate-300 shadow-lg'>
